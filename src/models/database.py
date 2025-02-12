b04496656a88de3d5068e18d184399e0cb2a1d51
Dima <strogansev.dima@yandex.ru>
2025-02-12 23:54:09 +0300

    feat(database): implement Base class

---
+++ src/models/database.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from models.config import settings
+
+
+DATABASE_URL = settings.database_url_asyncpg
+
+engine = create_async_engine(url=DATABASE_URL, echo=True)
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+
+
+fk_int = Annotated[int, mapped_column(ForeignKey("currencies.id", ondelete="CASCADE"))]
