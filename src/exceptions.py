89fbd9a1ca0912f05ce4525d128a7dea0873cd35
Dima <strogansev.dima@yandex.ru>
2025-02-27 16:43:59 +0300

    feat: add custom exceptions

---
+++ src/exceptions.py
@@ -0,0 +1,48 @@
+class BaseCurrencyException(Exception):
+    def __init__(self):
+        self.message = "Error"
+
+
+class CurrencyNotFoundError(BaseCurrencyException):
+    def __init__(self):
+        self.message = f"Валюта не найдена"
+
+
+# class MissingFormField(BaseCurrencyException):
+#     def __init__(self):
+#         self.message = f"Отсутствует нужное поле формы"
+
+
+class CurrencyCodeAlreadyExistsError(BaseCurrencyException):
+    def __init__(self):
+        self.message = f"Валюта с таким кодом уже существует"
+
+
+# class CurrencyPairMissingError(BaseCurrencyException):
+#     def __init__(self):
+#         self.message = f"Код валюты отсутствует в адресе"
+
+
+class ExchangeRateNotFoundError(BaseCurrencyException):
+    def __init__(self):
+        self.message = f"Обменный курс для пары не найден"
+
+
+class ExchangeCodeAlreadyExistsError(BaseCurrencyException):
+    def __init__(self):
+        self.message = f"Валютная пара с таким кодом уже существует"
+
+
+class CurrencyNotFoundException(BaseCurrencyException):
+    def __init__(self):
+        self.message = f"Одна (или обе) валюта из валютной пары не существует в БД"
+
+
+class CurrencyPairMissingException(BaseCurrencyException):
+    def __init__(self):
+        self.message = f"Валютная пара отсутствует в базе данных"
+
+
+class DatabaseUnavailableException(BaseCurrencyException):
+    def __init__(self):
+        self.message = f"База данных недоступна"
