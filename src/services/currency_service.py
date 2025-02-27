7a3e67af0b4e1950903b8386e4e6ffd3bd3d5e31
Dima <strogansev.dima@yandex.ru>
2025-02-27 16:54:57 +0300

    feat: add service, dependencies

---
+++ src/services/currency_service.py
@@ -0,0 +1,50 @@
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.exceptions import (
+    CurrencyNotFoundError,
+    CurrencyCodeAlreadyExistsError,
+)
+from src.repository.interface.currency_repository import CurrencyRepository
+from src.repository.interface.exchange_repository import ExchangeRepository
+from src.schemas import (
+    CurrencySchemas,
+    CurrencyCodeSchemas,
+    InCurrencySchemas,
+)
+from src.logger_config import logger
+
+
+class CurrencyService:
+    def __init__(self, currency_repo: CurrencyRepository):
+        self.currency_repo = currency_repo
+
+    async def find_all_currency(self, session: AsyncSession) -> list[CurrencySchemas]:
+        currency_sequence = await self.currency_repo.find_all(session=session)
+        return [
+            CurrencySchemas.model_validate(currency) for currency in currency_sequence
+        ]
+
+    async def find_one_or_none_currency(
+        self, currency_code: str, session: AsyncSession
+    ) -> CurrencySchemas:
+        validated_schema = CurrencyCodeSchemas(code=currency_code)
+        currency_record = await self.currency_repo.find_one_or_none(
+            session=session, filters=validated_schema
+        )
+        if currency_record is not None:
+            return CurrencySchemas.model_validate(currency_record)
+        raise CurrencyNotFoundError
+
+    async def create_one_currency(
+        self, currency: InCurrencySchemas, session: AsyncSession
+    ) -> CurrencySchemas:
+        try:
+            new_currency = await self.currency_repo.create_one(
+                session=session, filters=currency
+            )
+        except IntegrityError as e:
+            logger.error(f"Ошибка при создании валюты {e}")
+            raise CurrencyCodeAlreadyExistsError
+        logger.info(f"Валюта добавлена: {new_currency.fullname} - {new_currency.sign}")
+        return CurrencySchemas.model_validate(new_currency)
---
+++ src/services/exchange_service.py
@@ -0,0 +1,99 @@
+from decimal import Decimal
+
+from pydantic import ValidationError
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.exceptions import (
+    ExchangeRateNotFoundError,
+    ExchangeCodeAlreadyExistsError,
+    CurrencyNotFoundException,
+    CurrencyPairMissingException,
+)
+from src.repository.interface.currency_repository import CurrencyRepository
+from src.repository.interface.exchange_repository import ExchangeRepository
+from src.schemas import (
+    CurrencyCodeSchemas,
+    ExchangeRateSchemas,
+    InExchangeRateSchemas,
+    ExchangeRateAddSchemas,
+    ExchangeRateIDAddSchemas,
+)
+from src.logger_config import logger
+
+
+class ExchangeService:
+    def __init__(
+        self, currency_repo: CurrencyRepository, exchange_repo: ExchangeRepository
+    ):
+        self.currency_repo = currency_repo
+        self.exchange_repo = exchange_repo
+
+    async def find_all_exchange(
+        self, session: AsyncSession
+    ) -> list[ExchangeRateSchemas]:
+        exchange_sequence = await self.exchange_repo.find_all(session=session)
+        return [
+            ExchangeRateSchemas.model_validate(exchange)
+            for exchange in exchange_sequence
+        ]
+
+    async def find_one_or_none_exchange(
+        self, currency_code: str, session: AsyncSession
+    ) -> ExchangeRateSchemas:
+        base, target = currency_code[:3], currency_code[3:]
+        validated_schema = InExchangeRateSchemas(
+            base_currency=base, target_currency=target
+        )
+        exchange_record = await self.exchange_repo.find_one_or_none(
+            session=session, filters=validated_schema
+        )
+        logger.info(f"Запрос на валюты {exchange_record}")
+        if exchange_record is None:
+            raise ExchangeRateNotFoundError
+        return ExchangeRateSchemas.model_validate(exchange_record)
+
+    async def create_one_exchange(
+        self, session: AsyncSession, currency_exchange: ExchangeRateAddSchemas
+    ):
+        base_currency = await self.currency_repo.find_one_or_none(
+            session=session,
+            filters=CurrencyCodeSchemas(code=currency_exchange.base_currency),
+        )
+        target_currency = await self.currency_repo.find_one_or_none(
+            session=session,
+            filters=CurrencyCodeSchemas(code=currency_exchange.target_currency),
+        )
+        if base_currency is None or target_currency is None:
+            raise CurrencyNotFoundException
+
+        data = ExchangeRateIDAddSchemas(
+            base_currency_id=base_currency.id,
+            target_currency_id=target_currency.id,
+            rate=currency_exchange.rate,
+        )
+        try:
+            exchange_model = await self.exchange_repo.create_one(
+                session=session, filters=data
+            )
+        except IntegrityError as e:
+            logger.error(f"Ошибка при создании валютной пары {e}")
+            raise ExchangeCodeAlreadyExistsError
+
+        return ExchangeRateSchemas.model_validate(exchange_model)
+
+    async def update_exchange_pair(
+        self, currency_pair: str, rate: Decimal, session: AsyncSession
+    ) -> ExchangeRateSchemas:
+        base, target = currency_pair[:3], currency_pair[3:]
+        validated_schema = ExchangeRateAddSchemas(
+            base_currency=base, target_currency=target, rate=rate
+        )
+        exchange_record = await self.exchange_repo.update(
+            session=session, filters=validated_schema
+        )
+        try:
+            schema = ExchangeRateSchemas.model_validate(exchange_record)
+            return schema
+        except ValidationError as e:
+            raise CurrencyPairMissingException
