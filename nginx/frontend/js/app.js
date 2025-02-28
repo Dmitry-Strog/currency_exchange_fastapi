0de3a0a636f4109ec090d6e6fabdacbfed054f29
Dima <strogansev.dima@yandex.ru>
2025-02-28 18:30:43 +0300

    refactor: host

--- nginx/frontend/js/app.js
+++ nginx/frontend/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    const host = "http://localhost:8080"
+    const host = "http://0.0.0.0:8000"
 
     // Fetch the list of currencies and populate the select element
     function requestCurrencies() {
